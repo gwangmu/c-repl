15ee5f265dd92ef495909688d52b15dcbc1ec6cd
Gwangmu Lee <gwangmu.lee@cscs.ch>
2025-06-22 22:35:24 +0200

    Support array print

--- assets/print.h
+++ assets/print.h
@@ -3,30 +3,31 @@
 #include <stdio.h>
 #include <wchar.h>
 
-#define PRINTF_FORMAT(T)       \
-  _Generic( (T),               \
-    _Bool             : "%d",   \
-    char              : "%c",   \
-    signed char       : "%hhd", \
-    unsigned char     : "%hhu", \
-    short             : "%hd",  \
-    int               : "%d",   \
-    long              : "%ld",  \
-    long long         : "%lld", \
-    unsigned short    : "%hu",  \
-    unsigned int      : "%u",   \
-    unsigned long     : "%lu",  \
-    unsigned long long: "%llu", \
-    float             : "%f",   \
-    double            : "%f",   \
-    long double       : "%Lf",  \
-    char*             : "%s",   \
-    char const*       : "%s",   \
-    wchar_t*          : "%ls",  \
-    wchar_t const*    : "%ls",  \
-    void*             : "%p",   \
-    void const*       : "%p",   \
-    default           : ""      \
+#define PRINTF_FORMAT(T)          \
+  _Generic( (T),                  \
+    _Bool             : "%d",     \
+    char              : "'%c'",   \
+    signed char       : "%hhd",   \
+    unsigned char     : "%hhu",   \
+    short             : "%hd",    \
+    int               : "%d",     \
+    long              : "%ld",    \
+    long long         : "%lld",   \
+    unsigned short    : "%hu",    \
+    unsigned int      : "%u",     \
+    unsigned long     : "%lu",    \
+    unsigned long long: "%llu",   \
+    float             : "%f",     \
+    double            : "%f",     \
+    long double       : "%Lf",    \
+    char*             : "\"%s\"", \
+    char const*       : "\"%s\"", \
+    wchar_t*          : "\"%ls\"",\
+    wchar_t const*    : "\"%ls\"",\
+    void*             : "%p",     \
+    void const*       : "%p",     \
+    default           : ""        \
   )
 
-#define RAW_PRINT(X) dprintf(3, PRINTF_FORMAT((X)), (X))
+#define AUTO_PRINT(X) dprintf(3, PRINTF_FORMAT((X)), (X))
+#define RAW_PRINT(X) dprintf(3, (X))
